a3f551e9719111e7ae2aa4a29f90f1ec2ca7c804
soniharshil54 <sharshil43@yahoo.com>
2022-07-01 17:59:57 +0530

    dynamodb added on update api, all crud working with dynamodb

--- functions/api/deleteEnvironment.js
+++ functions/api/deleteEnvironment.js
@@ -1,29 +1,43 @@
+const AWS = require('aws-sdk');
+const docClient = new AWS.DynamoDB.DocumentClient();
+const { ENVIRONMENT_TABLE } = process.env;
+
 module.exports.handler = async (event) => {
   console.log('event', event);
   const params = event.pathParameters;
   const { id } = params;
   console.log('params', params);
 
-  const environments =  [
-    {
-      "id": "1",
-      "hospitalName": "AG",
-      "envName": "AG ENV",
-      "description": "AG DESC",
-    },
-    {
-      "id": "2",
-      "hospitalName": "Einstein Medical Center",
-      "envName": "Einstein Medical Center ENV",
-      "description": "Einstein Medical Center DESC",
-    },
-  ]
+  // const environments =  [
+  //   {
+  //     "id": "1",
+  //     "hospitalName": "AG",
+  //     "envName": "AG ENV",
+  //     "description": "AG DESC",
+  //   },
+  //   {
+  //     "id": "2",
+  //     "hospitalName": "Einstein Medical Center",
+  //     "envName": "Einstein Medical Center ENV",
+  //     "description": "Einstein Medical Center DESC",
+  //   },
+  // ]
 
-  const updatedEnvironments = environments.filter((env) => env.id !== id)
+  // const updatedEnvironments = environments.filter((env) => env.id !== id)
 
+  const deleteParams = {
+    TableName: ENVIRONMENT_TABLE,
+    Key: {
+      id,
+    },
+  };
+  console.log('deleteParams', deleteParams);
+  const deleteItemResponse = await docClient.delete(deleteParams).promise();
+  console.log('deleteItemResponse', deleteItemResponse);
   const responseBody = {
-    data: updatedEnvironments
+    data: {}
   }
+
   console.log('responseBody', responseBody);
   const response = {
     statusCode : 200,
